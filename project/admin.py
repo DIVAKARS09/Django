93442bd72a8045db0518e29891659d0b11643dd0
DIVAKAR S <140895967+DIVAKARS09@users.noreply.github.com>
2025-11-01 10:36:50 +0530

    Delete project directory

--- project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Item, Sales, UserProfile
-
-admin.site.register(Category)
-admin.site.register(Item)
-admin.site.register(Sales)
-admin.site.register(UserProfile)
